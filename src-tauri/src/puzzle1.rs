a98e1db6b59dd6b67613774f48f3e0704eade03c
plumberpumpkin <28160748+plumberpumpkin@users.noreply.github.com>
2023-12-20 11:43:06 +0100

    puzzle 1 input processing - first half completed

--- src-tauri/src/puzzle1.rs
+++ src-tauri/src/puzzle1.rs
@@ -2,12 +2,20 @@
 pub fn puzzle1(input: String) -> u16
 {
     let mut result: u16 = 0;
-    let entries: Vec<&str> = input.split(" ").collect();
+    let entries: Vec<&str> = input.split_whitespace().collect();
     for entry in entries{
-
         //store all numbers in new object
-        //build number from first and last digit
-        //add number to result
+        let mut numbers: Vec<char> = Vec::new();
+        for char in entry.chars(){
+            if char.is_digit(10){
+                numbers.push(char);
+            }
+        }
+        let mut digits: String = String::from("");
+        digits.push(numbers[0]);
+        digits.push(numbers[numbers.len()-1]);
+        let number: u16 = digits.parse().unwrap();
+        result += number;
     }
     return result;
 }
