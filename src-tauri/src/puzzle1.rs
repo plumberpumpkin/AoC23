21a4de2a13fe7b6e16514c146740614899e5a82e
plumberpumpkin <28160748+plumberpumpkin@users.noreply.github.com>
2023-12-20 14:31:39 +0100

    puzzle 1 input processing - advanced puzzle competed

--- src-tauri/src/puzzle1.rs
+++ src-tauri/src/puzzle1.rs
@@ -38,10 +38,73 @@ pub fn puzzle1advanced(input: String) -> u16
         }
         //now search for written numbers
         if entry.contains("one") {
-            let number_tuple: (String, usize) = ("1".to_string(), entry.find("one").unwrap());
-            number_tuples.push(number_tuple);
+            //Finds the first occurence of the pattern in the string
+            let number_tuple1: (String, usize) = ("1".to_string(), entry.find("one").unwrap());
+            //Finds the last occurence of the pattern in the string
+            let number_tuple2: (String, usize) = ("1".to_string(), entry.rfind("one").unwrap());
+            //Store them both to vector -> sorting by index value guarantees that double numbers are next to each other with the same index and the number builds correctly
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
+        }
+        if entry.contains("two") {
+            let number_tuple1: (String, usize) = ("2".to_string(), entry.find("two").unwrap());
+            let number_tuple2: (String, usize) = ("2".to_string(), entry.rfind("two").unwrap());
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
+        }
+        if entry.contains("three") {
+            let number_tuple1: (String, usize) = ("3".to_string(), entry.rfind("three").unwrap());
+            let number_tuple2: (String, usize) = ("3".to_string(), entry.find("three").unwrap());
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
+        }
+        if entry.contains("four") {
+            let number_tuple1: (String, usize) = ("4".to_string(), entry.rfind("four").unwrap());
+            let number_tuple2: (String, usize) = ("4".to_string(), entry.find("four").unwrap());
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
+        }
+        if entry.contains("five") {
+            let number_tuple1: (String, usize) = ("5".to_string(), entry.rfind("five").unwrap());
+            let number_tuple2: (String, usize) = ("5".to_string(), entry.find("five").unwrap());
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
+        }
+        if entry.contains("six") {
+            let number_tuple1: (String, usize) = ("6".to_string(), entry.rfind("six").unwrap());
+            let number_tuple2: (String, usize) = ("6".to_string(), entry.find("six").unwrap());
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
+        }
+        if entry.contains("seven") {
+            let number_tuple1: (String, usize) = ("7".to_string(), entry.rfind("seven").unwrap());
+            let number_tuple2: (String, usize) = ("7".to_string(), entry.find("seven").unwrap());
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
+        }
+        if entry.contains("eight") {
+            let number_tuple1: (String, usize) = ("8".to_string(), entry.rfind("eight").unwrap());
+            let number_tuple2: (String, usize) = ("8".to_string(), entry.find("eight").unwrap());
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
+        }
+        if entry.contains("nine") {
+            let number_tuple1: (String, usize) = ("9".to_string(), entry.rfind("nine").unwrap());
+            let number_tuple2: (String, usize) = ("9".to_string(), entry.find("nine").unwrap());
+            number_tuples.push(number_tuple1);
+            number_tuples.push(number_tuple2);
         }
 
+        //now sort the tuples by their index
+        number_tuples.sort_by(|a, b| a.1.cmp(&b.1));
+        println!("{:?}", number_tuples);
+       // number_tuples.reverse();
+        //now build the number
+        let mut number: String = String::from("");
+        number.push(number_tuples[0].0.chars().nth(0).unwrap());
+        number.push(number_tuples[number_tuples.len()-1].0.chars().nth(0).unwrap());
+        let number: u16 = number.parse().unwrap();
+        result += number;
 
     }
     return result;
